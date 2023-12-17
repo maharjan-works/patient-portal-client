6880e5815ccbd54a65f75e95d39f9859f2577b38
maharjan.works <maharjan.works@gmail.com>
2023-12-17 11:04:55 -0500

    component employee-list modified, class patient created

--- src/app/patient-list/patient-list.component.ts
+++ src/app/patient-list/patient-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from '../patient';
 
 @Component({
   selector: 'app-patient-list',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientListComponent implements OnInit {
 
+  patients: Patient[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.patients = [
+      {
+      "patientId": 100,
+      "firstName": "joseph",
+      "lastName": "biden",
+      "dob": "1942-11-20T04:00:00.000+00:00",
+      "email": "joe.biden@gmail.com",
+      "phoneNumber": " 202-456-1111"
+      },
+      {
+      "patientId": 101,
+      "firstName": "kamala",
+      "lastName": "harris",
+      "dob": "1962-10-20T04:00:00.000+00:00",
+      "email": "kamala.harris@gmail.com",
+      "phoneNumber": " 202-456-1111"
+      }
+      ]
   }
 
 }
---
+++ src/app/patient.ts
@@ -0,0 +1,8 @@
+export class Patient {
+    patientId: number = 0;
+    firstName: string = '';
+    lastName: string = '';
+    dob: string = '';
+    email: string = '';
+    phoneNumber: string ='';
+}
